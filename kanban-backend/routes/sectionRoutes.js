14d735b884dab6f15fe664872c97dcee79ec4984
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-05-11 16:45:45 +0530

    Update sectionRoutes.js

--- kanban-backend/routes/sectionRoutes.js
+++ kanban-backend/routes/sectionRoutes.js
@@ -1,9 +1,8 @@
-// routes/sectionRoutes.js
 const express = require('express');
 const router  = express.Router();
 const ctrl    = require('../controllers/sectionController');
 
-// Debug: confirm ctrl has the methods
+
 console.log('sectionController exports:', Object.keys(ctrl));
 
 router.get(   '/',    ctrl.getSections);
