6b551cef5f7cca3026a6d167f65483f7b628ef98
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-05-11 16:01:04 +0530

    Add files via upload

---
+++ kanban-frontend/src/api.js
@@ -0,0 +1,9 @@
+
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',  // your Express backend
+  headers: { 'Content-Type': 'application/json' }
+});
+
+export default api;
